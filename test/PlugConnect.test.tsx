e85f0172b66ec70709afd466be76d9b1b2562597
Helder Oliveira <heldrida@gmail.com>
2021-08-04 13:18:52 +0100

    test: 💍 plug connect renders

---
+++ test/PlugConnect.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import PlugConnect from '../src';
+
+describe('PlugConnect', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<PlugConnect
+      dark
+      onConnectCallback={() => {}}
+      title="Connect to Plug"
+      whitelist={[
+        'canister-1',
+        'canister-2'
+      ]}
+    />, div)
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});

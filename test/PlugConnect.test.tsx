b0943c3d0c0245de4756580f992bdde10d178d1c
aleacevedo <ale.acevedo@live.com.ar>
2022-11-08 18:27:19 -0300

    fix: tests

--- test/PlugConnect.test.tsx
+++ test/PlugConnect.test.tsx
@@ -1,3 +1,6 @@
+global.TextEncoder = require("util").TextEncoder;
+global.TextDecoder = require("util").TextDecoder;
+
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import PlugConnect from '../src';
